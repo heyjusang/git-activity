b9503ea30e28ada87c82c6bd8a232324eb33dcdd
Jusang Lee <co9901@wafflestudio.com>
2013-11-19 16:08:29 +0900

    draw arc

--- html/js/d3.gaugeGraph.js
+++ html/js/d3.gaugeGraph.js
@@ -1,31 +1,71 @@
 function drawGaugeGraph(gaugeId, options) {
-	var defaults = {
+  var defaults = {
+		margin: 50,
+		scale: {
+			startValue: 0,
+			endValue: 100,
+			ticks: [25, 50, 75],
+			colors: ["#76c8bd", "#f7c676", "#f7c676", "#c5819a"]
+		}
+  }
 
+
+  options = $.extend(true,{},defaults,options);
+
+  var width = $(gaugeId).width();
+  var height = $(gaugeId).height();
+  var centerX = width/2;
+  var centerY = height/2;
+  var radius = (Math.min(width,height) - options.margin) / 2;	
+
+
+
+	//draw chart
+  var chart = d3.select(gaugeId).append("svg")
+  .attr("class", "gaugeGraph")
+  .attr("width", width)
+  .attr("height", height);
+
+	//draw arc
+	
+	var startAngle = 0;
+	var endAngle;
+	var ticks = options.scale.ticks;
+	var colors = options.scale.colors;
+	var intervalAngle = 5;
+	for (var i=0; i < ticks.length; i++) {
+
+		var tick = ticks[i]; 
+
+		endAngle = (260 * tick) / (options.scale.endValue - options.scale.startValue);
+
+		var arc = d3.svg.arc()
+		.innerRadius(radius-2)
+		.outerRadius(radius)
+		.startAngle((startAngle - 130 + intervalAngle) * (Math.PI/180))
+		.endAngle((endAngle - 130 - intervalAngle) * (Math.PI/180))
+
+		chart.append("path")
+		.attr("d", arc)
+		.attr("transform", "translate("+ centerX + "," + centerY + ")")
+		.attr("fill", colors[Math.min(i, colors.length)]);
+
+		startAngle = endAngle;
 	}
 
-	var width,height,centerX,centerY;
+	endAngle = 260;
 
-	options = $.extend(true,{},defaults,options);
+	var arc = d3.svg.arc()
+	.innerRadius(radius-2)
+	.outerRadius(radius)
+	.startAngle((startAngle - 130 + intervalAngle) * (Math.PI/180))
+	.endAngle((endAngle - 130 - intervalAngle) * (Math.PI/180))
 
-	width = $(gaugeId).width();
-	height = $(gaugeId).height();
-	centerX = width/2;
-	centerY = height/2;
+	chart.append("path")
+	.attr("d", arc)
+	.attr("transform", "translate("+ centerX + "," + centerY + ")")
+	.attr("fill", colors[colors.length - 1]);
 
-	var chart = initialize(gaugeId);
-
-	function initialize(gaugeId) {
-		var chart = d3.select(gaugeId).append("svg")
-		.attr("class", "gaugeGraph")
-		.attr("width", width)
-		.attr("height", height);
-
-		return chart;
-	}
-
-	function drawArc(chart, options) {
-
-	}
 
 }
 
