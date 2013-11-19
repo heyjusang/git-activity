e3b2834d42616964a55078cb327959b23a377572
Jusang Lee <co9901@wafflestudio.com>
2013-11-19 17:31:45 +0900

    use d3

--- html/js/chart.js
+++ html/js/chart.js
@@ -1,4 +1,3 @@
-
 function parseData(data, valueMax) {
     var result = [];
     for (var i = 0; i < data.length; i++)
@@ -7,134 +6,36 @@ function parseData(data, valueMax) {
 }
 
 function drawChart(chartId, dataSource, valueMin, valueMax) {
-    $(chartId).dxChart({
-        dataSource: dataSource,
-        commonSeriesSettings: {
-            type: "spline",
-            argumentField: "index",
-            color: "#7cd2c7"
-        },
-        commonAxisSettings: {
-            grid: {
-                visible: true
-            }
-        },
-        argumentAxis: {
-            title: {
-                margin: 2,
-                text: "Month",
-                font: {
-                    size: 13
-                }
-            },
-            valueMarginsEnabled: false,
-            min: 0,
-            max: dataSource.length - 1,
-            tickInterval: 1
-        },
-        valueAxis: {
-            valueMarginsEnabled: false,
-            min: valueMin,
-            max: valueMax,
-            tickInterval: (valueMax - valueMin) / 4
-        },
-        series: {
-            valueField: "value", 
-            point: {
-                size: 0
-            }
-        },
-        tooltip: {
-            enabled: true
-        },
-        legend: {
-            visible: false
-        },
-        commonPaneSettings: {
-            border: {
-                visible: true,
-                bottom: false
-            }
-        }
-    });
-};
-
-function drawGauge(gaugeId, value, valueMin, valueMax) {
-    $(gaugeId).dxCircularGauge({
-        margin: {
-            left: 10,
-            right: 10,
-            top: 10,
-            bottom: 10
-        },
-        containerBackgroundColor: "#363e5b",
-        spindle: {
-            color: "#ffffff"
-        },
-        scale: {
-            startValue: valueMin,
-            endValue: valueMax,
-            majorTick: {
-                color: 'none',
-                tickInterval : (valueMax - valueMin) / 4
-            },
-            label: {
-                font: {
-                    color: '#a7acbc',
-                    indentFromTick: 8
-                }
-            },
-        },
-        rangeContainer: {
-            width :3,
-            backgroundColor: 'none',
-            ranges: [
-                {
-                    startValue: valueMin,
-                    endValue : valueMin + (valueMax - valueMin) / 4 - (valueMax - valueMin) / 40,
-                    color: '#76c8bd'
-                },
-                {
-                    startValue: valueMin + (valueMax - valueMin) / 4 + (valueMax - valueMin) / 40,
-                    endValue : valueMin + (valueMax - valueMin) / 2 - (valueMax - valueMin) / 40,
-                    color: '#f7c676'
-                },
-                {
-                    startValue: valueMin + (valueMax - valueMin) / 2 + (valueMax - valueMin) / 40,
-                    endValue : valueMin + (valueMax - valueMin) * 3 / 4 - (valueMax - valueMin) / 40,
-                    color: '#f7c676'
-                },
-                {
-                    startValue: valueMin + (valueMax - valueMin) * 3 / 4 + (valueMax - valueMin) / 40,
-                    endValue : valueMax,
-                    color: '#c5819a'
-                },
-            ]
-        },
-        needles: [
-            {
-                offset: 5,
-                indentFromCenter: 7,
-                value: value 
-            }
-        ],
-        preset: 'preset1'
-    });
-};
-
-function processAll(prefix, data, valueMin, valueMax) {
-    var source = parseData(data, valueMax);
-    drawChart(prefix + " .col3", source, valueMin, valueMax);
-    drawGauge(prefix + " .circularGauge", source[source.length-1].value, valueMin, valueMax);
-    $(prefix + " .label-value").animateNumbers(source[source.length-1].value);
+	drawLineGraph(chartId,{
+		data : dataSource});
 }
 
-$(window).load(function() {
-    $('.header-text').text("Project Activity - " + data.name);
-    if (data.scf.length > 0) {
-        processAll("#scf", data.scf, 0, 16);
-        processAll("#rcf", data.rcf, 0, 100);
-        processAll("#ruci", data.ruci, 0, 100);
-        processAll("#ccr", data.ccr, 0, 100);
+function drawGauge(gaugeId, value, valueMin, valueMax) {
+  range = valueMax-valueMin;
+	drawGaugeGraph(gaugeId, {
+    data: value,
+    section: {
+      startValue: valueMin,
+      endValue: valueMax,
+      ticks: [range/4, range/2, 3*range/4]
     }
+	});
+}
+
+function processAll(prefix, data, valueMin, valueMax) {
+	var source = parseData(data, valueMax);
+	drawChart(prefix + " .col3", data, valueMin, valueMax);
+	drawGauge(prefix + " .circularGauge", data[data.length-1], valueMin, valueMax);
+	$(prefix + " .label-value").animateNumbers(Number(data[data.length-1].toFixed(2)));
+}
+
+
+$(window).load(function() {
+	$('.header-text').text("Project Activity - " + data.name);
+	if (data.scf.length > 0) {
+		processAll("#scf", data.scf, 0, 16);
+		processAll("#rcf", data.rcf, 0, 100);
+		processAll("#ruci", data.ruci, 0, 100);
+		processAll("#ccr", data.ccr, 0, 100);
+	}
 });
