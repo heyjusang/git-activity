d84e783d04429b8e5e21139ccfd8bef7db4e1fb3
Jusang Lee <co9901@wafflestudio.com>
2013-11-21 17:14:10 +0900

    gaugeGraph - interval value

--- html/js/d3.gaugeGraph.js
+++ html/js/d3.gaugeGraph.js
@@ -1,6 +1,6 @@
 function drawGaugeGraph(gaugeId, options) {
 	var defaults = {
-		margin: 50,
+		margin: 25,
 		data: 30,
 		section: {
 			startValue: 0,
@@ -35,7 +35,7 @@ function drawGaugeGraph(gaugeId, options) {
 	var height = $(gaugeId).height();
 	var centerX = width/2;
 	var centerY = height/2;
-	var radius = (Math.min(width,height) - options.margin) / 2;	
+	var radius = Math.min(width,height) / 2 - options.margin;	
 
 	//draw chart
 	var chart = d3.select(gaugeId).append("svg")
@@ -67,6 +67,16 @@ function drawGaugeGraph(gaugeId, options) {
 		.attr("transform", "translate("+ centerX + "," + centerY + ")")
 		.attr("fill", colors[Math.min(i, colors.length)]);
 
+    chart.append("text")
+    .text(tick)
+    .attr("x", centerX + (radius + options.interval.padding) * Math.sin(endAngle*Math.PI/180))
+    .attr("y", centerY - (radius + options.interval.padding) * Math.cos(endAngle*Math.PI/180))
+    .attr("dy", 5)
+    .attr("text-anchor", options.interval.textAnchor)
+    .attr("fill", options.interval.fontColor)
+    .attr("font-size", options.interval.fontSize);
+
+
 		startAngle = endAngle;
 	}
 
@@ -81,41 +91,59 @@ function drawGaugeGraph(gaugeId, options) {
 	chart.append("path")
 	.attr("d", arc)
 	.attr("transform", "translate("+ centerX + "," + centerY + ")")
-	.attr("fill", colors[colors.length - 1]);
+  .attr("fill", colors[colors.length - 1]);
 
-	//draw spindle
+  chart.append("text")
+  .text(options.section.startValue)
+  .attr("x", centerX + (radius + options.interval.padding) * Math.sin(options.section.startAngle*Math.PI/180))
+  .attr("y", centerY - (radius + options.interval.padding) * Math.cos(options.section.startAngle*Math.PI/180))
+  .attr("dy", 5)
+  .attr("text-anchor", options.interval.textAnchor)
+  .attr("fill", options.interval.fontColor)
+  .attr("font-size", options.interval.fontSize);
 
-	var spindle = d3.svg.arc()
-	.innerRadius(options.spindle.radius - options.spindle.strokeWidth)
-	.outerRadius(options.spindle.radius)
-	.startAngle(0)
-	.endAngle(360 * (Math.PI/180));
+  chart.append("text")
+  .text(options.section.endValue)
+  .attr("x", centerX + (radius + options.interval.padding) * Math.sin(endAngle*Math.PI/180))
+  .attr("y", centerY - (radius + options.interval.padding) * Math.cos(endAngle*Math.PI/180))
+  .attr("dy", 5)
+  .attr("text-anchor", options.interval.textAnchor)
+  .attr("fill", options.interval.fontColor)
+  .attr("font-size", options.interval.fontSize);
 
-	chart.append("path")
-	.attr("d", spindle)
-	.attr("transform", "translate("+ centerX + "," + centerY + ")")
-	.attr("fill", options.spindle.strokeColor);
+  //draw spindle
 
-	//draw needle
+  var spindle = d3.svg.arc()
+  .innerRadius(options.spindle.radius - options.spindle.strokeWidth)
+  .outerRadius(options.spindle.radius)
+  .startAngle(0)
+  .endAngle(360 * (Math.PI/180));
 
-	targetAngle = options.section.startAngle + ((options.section.endAngle - options.section.startAngle) * options.data) / (options.section.endValue - options.section.startValue);
+  chart.append("path")
+  .attr("d", spindle)
+  .attr("transform", "translate("+ centerX + "," + centerY + ")")
+  .attr("fill", options.spindle.strokeColor);
 
-	var needle = chart.append("line")
-	.attr("x1", centerX)
-	.attr("x2", centerX)
-	.attr("y1",(centerY - options.spindle.radius))
-	.attr("y2",(centerY - radius + options.needle.padding))
-	.style("stroke", options.needle.needleColor)
-	.style("stroke-width", options.needle.needleWidth);
+  //draw needle
 
-	needle
-	.transition()
-	.duration(1000)
-	.ease("linear")
-	.attrTween("transform", tween);
+  targetAngle = options.section.startAngle + ((options.section.endAngle - options.section.startAngle) * options.data) / (options.section.endValue - options.section.startValue);
 
-	function tween(d, i, a) {
-		return d3.interpolateString("rotate(" + options.section.startAngle + ", " + centerX + ", " + centerY + ")", "rotate(" + targetAngle + ", " + centerX + ", " + centerY + ")");
-	}
+  var needle = chart.append("line")
+  .attr("x1", centerX)
+  .attr("x2", centerX)
+  .attr("y1",(centerY - options.spindle.radius))
+  .attr("y2",(centerY - radius + options.needle.padding))
+  .style("stroke", options.needle.needleColor)
+  .style("stroke-width", options.needle.needleWidth);
+
+  needle
+  .transition()
+  .duration(1000)
+  .ease("linear")
+  .attrTween("transform", tween);
+
+  function tween(d, i, a) {
+    return d3.interpolateString("rotate(" + options.section.startAngle + ", " + centerX + ", " + centerY + ")", "rotate(" + targetAngle + ", " + centerX + ", " + centerY + ")");
+  }
 }
 
