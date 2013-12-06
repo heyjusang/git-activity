882c4d10d13ff4f50056873c8553dc1835a574da
Daegeun Lee <elnn@elnn.kr>
2013-12-06 21:45:28 +0900

    Remove some files & Support robust regression

--- src/predict.py
+++ src/predict.py
@@ -3,12 +3,6 @@ import json
 import numpy as np
 from sklearn.gaussian_process import GaussianProcess
 
-def normalize(e):
-    return 100.*(1-1./(1+e**2))
-
-def denormalize(e):
-    return np.sqrt(-1+1/(1-e/100.))
-
 # Prepare training data for regression
 data = json.load(open('html/js/data.js'))
 y = np.array(data['activity'])
@@ -16,7 +10,8 @@ x = np.atleast_2d(xrange(len(y))).T
 
 # Instanciate a Gaussian Process model
 # Fit to data using Maximum Likelihood Estimation of the parameters
-gp = GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1)
+gp = GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1, normalize=True,
+                     nugget=1.0, random_start=10, random_state=486)
 gp.fit(x, y)
 
 # Generate the input space and make the prediction
