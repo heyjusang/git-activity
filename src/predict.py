c1c9a5f904fe6adfd598d5015b3498308cdab6a9
Daegeun Lee <elnn@elnn.kr>
2013-12-16 21:20:33 +0900

    Change default unit to MONTH

--- src/predict.py
+++ src/predict.py
@@ -3,21 +3,25 @@ import json
 import numpy as np
 from sklearn.gaussian_process import GaussianProcess
 
-# Prepare training data for regression
 data = json.load(open('html/js/data.js'))
-y = np.array(data['activity'])
-x = np.atleast_2d(xrange(len(y))).T
 
-# Instanciate a Gaussian Process model
-# Fit to data using Maximum Likelihood Estimation of the parameters
-gp = GaussianProcess(theta0=1e-1, nugget=1e-6)
-gp.fit(x, y)
+if len(data['activity']) > 0:
+    # Prepare training data for regression
+    y = np.array(data['activity'])
+    x = np.atleast_2d(xrange(len(y))).T
 
-# Generate the input space and make the prediction
-xp = np.atleast_2d(xrange(len(y), len(y)+6)).T
-yp = gp.predict(xp).clip(0.0, 100.0)
+    # Instanciate a Gaussian Process model
+    # Fit to data using Maximum Likelihood Estimation of the parameters
+    gp = GaussianProcess(theta0=1e-1, nugget=1e-6)
+    gp.fit(x, y)
 
-# Save the regression result
-data['future'] = list(yp)
+    # Generate the input space and make the prediction
+    xp = np.atleast_2d(xrange(len(y), len(y)+6)).T
+    yp = gp.predict(xp).clip(0.0, 100.0)
+
+    # Save the regression result
+    data['future'] = list(yp)
+else:
+    data['future'] = []
 out = 'var data = %s;' % json.dumps(dict(data))
 open('html/js/data.js','w').write(out)
